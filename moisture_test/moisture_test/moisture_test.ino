682380e1efc2fb53a5480f80611f08e0bd8d0c98
Stephan <stephan_wink@web.de>
2018-07-03 22:24:53 +0200

    first working prototype

--- moisture_test/moisture_test/moisture_test.ino
+++ moisture_test/moisture_test/moisture_test.ino
@@ -12,7 +12,7 @@
 void setup() 
 {
   pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-  Serial.begin(9600);               // open serial port, set the baud rate as 9600 bps
+  Serial.begin(115200);             // open serial port, set the baud rate as 115200 bps
 }
 
 // the loop function runs over and over again forever
@@ -22,9 +22,8 @@ void loop()
   digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
                                     // but actually the LED is on; this is because 
                                     // it is acive low on the ESP-01)
-  val = analogRead(0);              //connect sensor to Analog 0
-  Serial.print(val);                //print the value to serial port                                 
-  delay(1000);                      // Wait for a second
+  val = analogRead(0);              //connect sensor to Analog 0                  
+  Serial.println(val);              // print the value to serial port                               
   digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+  delay(1000);                      // Wait for two seconds (to demonstrate the active low LED)
 }
