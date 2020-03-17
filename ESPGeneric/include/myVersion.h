38ce5ca29226bb57e198d61593efb1ec2597fd2d
Stephan Wink <stephan_wink@web.de>
2020-03-17 19:39:15 +0100

    changed version tracking to platformio

--- ESPGeneric/include/myVersion.h
+++ ESPGeneric/include/myVersion.h
@@ -31,50 +31,6 @@ vAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 * 
 * AUTHOR :    Stephan Wink        START DATE :    01.10.2017
 *
-*
-* REF NO  VERSION DATE    WHO     DETAIL
-* 000       16.10         SWI     branched from template
-* 001       21.11         SWI     first version with single relay object
-* 002       01.12         SWI     first version running with SONOFF switch
-* 003       03.12         SWI     added dev short description to config ssid
-* 004       03.12         SWI     minor bug fixes
-* 005       05.12         SWI     load default params from eeprom before starting 
-*                                 config portal
-* 006       07.12         SWI     add broadcast command topic       
-*                                 add command in dht device to toggle power save mode
-* 007       10.12         SWI     rework single relay to support multi relays
-*                                 add device list in v1 to support multi devices
-* 008       03.01         SWI     changed config ssio
-* 009       06.01         SWI     bug fixing
-* 010       14.01         SWI     reworked mqtt structure and trace module rework
-* 011       14.01         SWI     reworked single relay command structure
-*                                 reworked trace commands in gen
-* 012       18.01         SWI     implement new generic topics to read/write settings                                
-*                                 without wifimanager involvement
-* 013       21.01         SWI     inserted room parameter for more identification
-* 014       21.01         SWI     set dht update rate to 30secs
-* 015       28.01         SWI     rework trace topic structure
-* 016       11.02         SWI     add Generic GPIO device and MCP23017 I2C
-* 017       15.05         SWI     add capability for 8 channel relay
-* 018       30.05         SWI     rework of bme and dht sensors to handle flexible report
-*                                 cycle times and removed power save feature from these
-*                                 sensors, this is now handled by the PowerSave device
-* 019       09.06         SWI     changed mcp 8 reley to double dht, changed pins for                                 
-*                                 esp relay 8
-* 020       06.07         SWI     integrated Sen0193 capacitive moisture sensor  
-* 021       02.08         SWI     add additional pir pin verification because of 
-*                                 some wired behaviour of the pir in dev03    
-* 022       25.08         SWI     introduce bcast and trace/capability write commands                         
-* 023       19.09         SWI     introduce utils and DimLight device
-* 024       24.09         SWI     reworked the DimLight device to more standardize
-* 025       04.10         SWI     introduced neo pixels support
-* 026       07.11         SWI     migrated to platformio, add max digit in DimLight, add
-                                    3D printer control device
-* 027       06.01         SWI     changed polarity of 3D control relays
-* 028       02.01.2020    SWI     add Generic Sensor device, used for health monitoring
-* 029       18.01         SWI     updated arduino core library and WifiManager library
-*                                 generated MultiSense + Relay One device
-* 030       16.02         SWI     add generic ota to the firmware
 *****************************************************************************************/
 #ifndef MYVERSION_H
 #define MYVERSION_H
